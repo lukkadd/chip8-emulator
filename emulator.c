77e09a6db568d1e8f9bae4cd3d07e566e21f610f
lukkadd <lukkadd@yahoo.com.br>
2017-10-29 08:57:09 -0200

    initialized the repo

---
+++ emulator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+chip8 myChip8;
+
+int main(int argc, char const *argv[])
+{
+	//external graphics stuff I/O
+	setupGraphics();
+	setupInput();
+
+	//chip8 intialization stuff
+
+	return 0;
+}
