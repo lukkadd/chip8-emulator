e6882df340dfc8f176df457e48672eabbb193686
lukkadd <lukkadd@yahoo.com.br>
2017-10-29 10:12:02 -0200

    added chip8::emulatecycle and made some progress on initialize

--- chip8.c
+++
@@ -1,7 +0,0 @@
-void chip8::initialize(){
-	//initializing registers and memory
-	pc = 0x200; //pc starts at position 512 where the game is loaded
-	opcode = 0; //clear current opcode
-	I = 0;	//clear index register
-	sp = 0; //clear stack pointer
-}
